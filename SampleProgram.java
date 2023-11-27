a27dac2d7ab6f1ea6cfbdd112e158a936913763b
marishka-m <145405382+marishka-m@users.noreply.github.com>
2023-11-27 17:28:05 -0500

    Create a sample Java program
    Created a sample java program for this repository.

---
+++ SampleProgram.java
@@ -0,0 +1,17 @@
+public class SampleProgram {
+
+    public static void main(String[] args) {
+        //Print our Hello world message
+        System.out.println("Hello, World!");
+
+        // Print numbers from 1 to 100
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+        // Print fun message
+        System.out.println("This is fun! I'm new to GitHub");
+        //Print another fun message 
+        System.out.println("This is coooooooooooooooooool");
+    }
+}
+
